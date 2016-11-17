90bebec3d44293c9903b9d8f10480256f73a466b
Legionu4 <Rkuzmovych@gmail.com>
2016-11-17 17:17:40 +0200

    some old changes

--- Olbp/App_Start/BundleConfig.cs
+++ Olbp/App_Start/BundleConfig.cs
@@ -20,8 +20,7 @@ namespace Olbp
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/MyScripts.js"));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       //"~/Content/bootstrap-cerulean.css",
